08b6de82b8823886aeb91047d6d8f60353d95e61
Shreyash Satadeve <154428618+Shrey3satdeve@users.noreply.github.com>
2026-01-07 00:01:22 +0530

    Delete backend directory

--- backend/routes/orders.routes.js
+++
@@ -1,9 +0,0 @@
-// routes/orders.routes.js
-const express = require('express');
-const router = express.Router();
-const ordersController = require('../controllers/orders.controller');
-
-router.get('/orders', ordersController.listOrders);
-router.post('/orders', ordersController.createOrder);
-
-module.exports = router;
